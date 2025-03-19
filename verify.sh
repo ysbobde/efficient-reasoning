a5db4a45aa8af785f36dbb0ba668a1d68a774e0b
ysbobde <ysbobde@icloud.com>
2025-03-18 23:24:56 -0400

    Revert "Revert "major change""
    This reverts commit a0850a5b231c81f53f5d3e4125f89c5b58b697ce.

--- verify.sh
+++ verify.sh
@@ -18,4 +18,4 @@ python evaluate_response.py \
   --model_path=Qwen/Qwen2.5-Math-1.5B-Instruct \
   --task verify \
   --tokenizer =Qwen/Qwen2.5-Math-1.5B-Instruct \
-  --prompt_name =verifier_math1
+  --prompt_name =verifier1
